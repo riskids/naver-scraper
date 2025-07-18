4ccf1ce07baca2418e44c1cfbe84b6a4b92336d0
Riski D Sabariyanto <dwisriski@yahoo.com>
2025-07-18 23:10:37 +0700

    update: add default route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import { Application } from "express";
 
 export default class Routes {
   constructor(app: Application) {
-    app.use("/api/naver", naverRoutes)
+    app.use("/api/naver", naverRoutes);
+
+    app.get("/", (req, res) => {
+      res.json({ status: "Application running" });
+    });
   }
 }
